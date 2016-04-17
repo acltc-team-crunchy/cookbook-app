028b3112944f605f7009adff39fe50a6c66daca4
Mark Richardson <markeban@yahoo.com>
2016-04-17 10:13:06 -0700

    finished new and create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params[:id])
   end
 
+  def new
+    
+  end
+
+  def create
+    new_recipe = Recipe.new(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions], prep_time: params[:prep_time])
+    new_recipe.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'recipes#index'
   get "/recipes", to: 'recipes#index'
+  get "/recipes/new", to: 'recipes#new'
   get "/recipes/:id", to: 'recipes#show'
+  post "/recipes", to: 'recipes#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
