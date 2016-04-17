8d27d8964a9d10ecc4634f8811ef8427c28a1d9d
Mark Richardson <markeban@yahoo.com>
2016-04-17 13:29:24 -0700

    finished 7 RESTful routes with actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,39 @@ class RecipesController < ApplicationController
 
   def create
     new_recipe = Recipe.new(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions], prep_time: params[:prep_time])
+    puts "down there"
+    p new_recipe
     new_recipe.save
+  end
+
+  def edit
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def update
+
+    #First Way
+    # @recipe = Recipe.find_by(id: params[:id])
+    # @recipe.assign_attributes(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions], prep_time: params[:prep_time])
+    # @recipe.save
+
+    #Second Way
+    # @recipe = Recipe.find_by(id: params[:id])
+    # @recipe.title = params[:title]
+    # @recipe.chef = params[:chef]
+    # @recipe.ingredients = params[:ingredients]
+    # @recipe.directions = params[:directions]
+    # @recipe.prep_time = params[:prep_time]
+    # @recipe.save
+
+    #Third Way
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(title: params[:title], chef: params[:chef], ingredients: params[:ingredients], directions: params[:directions], prep_time: params[:prep_time])
+  end
+
+  def destroy
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get "/recipes/new", to: 'recipes#new'
   get "/recipes/:id", to: 'recipes#show'
   post "/recipes", to: 'recipes#create'
+  get "/recipes/:id/edit", to: 'recipes#edit'
+  patch "/recipes/:id", to: 'recipes#update'
+  delete "/recipes/:id", to: 'recipes#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
