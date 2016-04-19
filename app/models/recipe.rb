45238ea303efa1689b78ff53cb972c50ab0f6ae5
Mark Richardson <markeban@yahoo.com>
2016-04-18 18:35:01 -0700

    added model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ActiveRecord::Base
+
+  def purple_hippo
+    ingredients.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def friendly_prep_time
+    minutes = prep_time / 60
+    seconds = prep_time % 60
+    p minutes
+    p seconds
+    result = ""
+    result += "#{minutes} minutes" if minutes > 0
+    result += "#{seconds} seconds"  if seconds > 0
+    p result
+  end
+
 end
